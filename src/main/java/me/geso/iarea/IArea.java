abfd6cf03fd831400df36c9ae5720cda04cf42e9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-05-06 14:51:55 +0900

    comments

--- src/main/java/me/geso/iarea/IArea.java
+++ src/main/java/me/geso/iarea/IArea.java
@@ -1,5 +1,9 @@
 package me.geso.iarea;
 
+/**
+ * iArea class
+ * <a href="https://www.nttdocomo.co.jp/service/developer/make/content/iarea/domestic/index.html">オープンiエリア</a>
+ */
 public class IArea {
 	private String region;
 	private String prefecture;
@@ -13,18 +17,30 @@ public class IArea {
 		this.code = code;
 	}
 
+	/**
+	 * Get region name
+	 */
 	public String getRegion() {
 		return region;
 	}
 
+	/**
+	 * Get prefecture name
+	 */
 	public String getPrefecture() {
 		return prefecture;
 	}
 
+	/**
+	 * Get area name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Get area code
+	 */
 	public String getCode() {
 		return code;
 	}
--- src/main/java/me/geso/iarea/IAreaRepository.java
+++ src/main/java/me/geso/iarea/IAreaRepository.java
@@ -4,12 +4,25 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Created by tokuhirom on 5/6/15.
+ * IArea data repository.
  */
 public interface IAreaRepository {
+	/**
+	 * Get list of regions
+	 */
 	List<String> getRegions();
 
+	/**
+	 * Get list of prefectures
+	 */
 	List<String> getPrefectures();
 
+	/**
+	 * Get area object by lat/lng.
+	 *
+	 * @param latWGS84Degree latitude(緯度) in WGS84, Degree.
+	 * @param lngWGS84Degree longitude(経度) in WGS84, Degree.
+	 * @return iArea object for specified lat/lng.
+	 */
 	Optional<IArea> getAreaByWgs84LatLng(double latWGS84Degree, double lngWGS84Degree);
 }
